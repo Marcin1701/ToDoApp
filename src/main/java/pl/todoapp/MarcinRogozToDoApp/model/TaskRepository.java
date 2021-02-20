8193aae776074ecab779a8498c47605d62874128
Marcin1701 <marcin.rogoz1@outlook.com>
2021-02-20 11:25:01 +0100

    3. HATEOS

--- src/main/java/pl/todoapp/MarcinRogozToDoApp/model/TaskRepository.java
+++ src/main/java/pl/todoapp/MarcinRogozToDoApp/model/TaskRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 // Klasa służy do komunikacji z bazą danych
 //     Jest to interfejs        Rozszerza             1. Jaka encja 2. Jaki typ klucza
 // Udostępnienie repozytorium
-@RepositoryRestResource
+// RepositoryRestResource() -> z nawiasami to HATEOS
+@RepositoryRestResource(path = "todos")
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
     // Jpa repository - mamy operacja findAll
@@ -15,5 +16,9 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
     // Jak sprawdzić? - PPM na JpaRepository - Goto - super class
 
     // Wystarczy interfejs bazowy, żeby udostępnić wszystkie operacje
+
+    // HATEOS
+    // Stan aplikacji reprezentowane poprzez hipermedia, informacje otrzymywane przez _ (podkreślenie)
+    // Np. _links, _embedded
 
 }
