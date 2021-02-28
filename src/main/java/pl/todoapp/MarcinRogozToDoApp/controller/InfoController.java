665eb9ddf18a494a50b5cfecbafa35dc03227a7e
Marcin1701 <marcin.rogoz1@outlook.com>
2021-02-28 12:23:01 +0100

    16. @Autowired

--- src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/InfoController.java
+++ src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/InfoController.java
@@ -1,6 +1,8 @@
 package pl.todoapp.MarcinRogozToDoApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class InfoController {
 
     // Url do bazy danych
-    @Value("${spring.datasource.url}")
-    private String url;
+    // @Value("${spring.datasource.url}")
+    // private String url;
+    // Opcja z autowired - niezbyt dobra - jesteśmy zależni od springa
+    @Autowired
+    private DataSourceProperties dataSource;
+
     @Value("${my.prop}")
     private String myProp;
 
     @GetMapping("/info/url")
     String url() {
-        return url;
+        return dataSource.getUrl();
     }
 
     @GetMapping("/info/prop")
