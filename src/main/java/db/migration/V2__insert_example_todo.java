686150237e8437f94e3b894c4e90321216f377f6
Marcin1701 <marcin.rogoz1@outlook.com>
2021-03-05 20:26:30 +0100

    21. Migracja Java

---
+++ src/main/java/db/migration/V2__insert_example_todo.java
@@ -0,0 +1,15 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+public class V2__insert_example_todo extends BaseJavaMigration {
+
+    @Override
+    public void migrate(final Context context) {
+        new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true))
+                .execute("insert into tasks (description, done) values ('flyway_task', true)");
+    }
+}
