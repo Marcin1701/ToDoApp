090b5a87a6f490d6829cb12e319cf9a55f4ab2a1
Marcin1701 <marcin.rogoz1@outlook.com>
2021-03-21 13:27:56 +0100

    48. Lekkie testy integracyjne, @WebMvcTest, @MockBean

---
+++ src/test/java/pl/todoapp/MarcinRogozToDoApp/controller/TaskControllerLightIntegrationTest.java
@@ -0,0 +1,61 @@
+package pl.todoapp.MarcinRogozToDoApp.controller;
+
+// Testy integracyjne
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.todoapp.MarcinRogozToDoApp.model.Task;
+import pl.todoapp.MarcinRogozToDoApp.model.TaskRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+// Lekki test integracyjny
+// Podpinamy nasz kontroler
+@WebMvcTest(TaskController.class)
+public class TaskControllerLightIntegrationTest {
+
+    // CIęzko uzyskać task repository z kontekstu
+
+    // Klasa ułatwia testy
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Repozytorium fałszywe - trzymające wszsytko w pamięci
+    // Mockowanie beana - nie korzystamy z integration profilu
+    @MockBean
+    private TaskRepository repo;
+
+    // Odczyt konkretnego taska
+    @Test
+    void httpGet_returnsGivenTask() {
+        // Dane
+        when(repo.findById(anyInt()))
+                .thenReturn(Optional.of(new Task("foo", LocalDateTime.now())));
+
+        // Spodziewane
+        // Bezpośrednio pod URL
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.get("/tasks/123"))
+                    .andDo(print())
+                    .andExpect(content().string(containsString("\"description\":\"foo\"")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
