073916786d5a84c73dcadc2e2107d27e5ec8b9ca
Marcin1701 <marcin.rogoz1@outlook.com>
2021-03-23 10:50:26 +0100

    52. Wynoszenie części wspólnej z mappingu

--- src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/InfoController.java
+++ src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/InfoController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.todoapp.MarcinRogozToDoApp.TaskConfigurationProperties;
 
 @RestController
+@RequestMapping("/info")
 public class InfoController {
 
     // Url do bazy danych
@@ -29,12 +31,12 @@ public class InfoController {
         this.myProp = myProp;
     }
 
-    @GetMapping("/info/url")
+    @GetMapping("/url")
     String url() {
         return dataSource.getUrl();
     }
 
-    @GetMapping("/info/prop")
+    @GetMapping("/prop")
     boolean myProp() {
         return myProp.getTemplate().isAllowMultipleTasks();
         //return myProp.isAllowMultipleTasksFromTemplate();
