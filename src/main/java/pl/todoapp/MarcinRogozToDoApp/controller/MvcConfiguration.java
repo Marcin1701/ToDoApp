5be902000eb9a25aa03aeb9cc9141153d3561bde
Marcin1701 <marcin.rogoz1@outlook.com>
2021-03-23 11:45:35 +0100

    55. Wstrzykiwanie wielu interceptorów na raz

--- src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/MvcConfiguration.java
+++ src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/MvcConfiguration.java
@@ -1,19 +1,29 @@
 package pl.todoapp.MarcinRogozToDoApp.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Set;
+
 // Klasa konfiguracyjna, żeby zadziałał Interceptors
+// Tutaj możemy wstrzykiwać interceptory jako beany
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+
+    // Wszystkie możliwe interceptory
+    private Set<HandlerInterceptor> interceptors;
+
+    public MvcConfiguration(final Set<HandlerInterceptor> interceptors) {
+        this.interceptors = interceptors;
+    }
 
     // Dodawanie nowych interceptorów
     // W łąńcuchach filtrów - spring najpierw honoruje JEE czyli LoggerFilter, potem Interceptor
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
+        // Dodajemy od razu wszystkie interceptory
+        interceptors.forEach(registry::addInterceptor);
     }
-
-
 }
