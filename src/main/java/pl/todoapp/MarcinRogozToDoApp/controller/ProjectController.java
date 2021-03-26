4d91658871ccd8296914f3abf84d3f8b3662cb7e
Marcin1701 <marcin.rogoz1@outlook.com>
2021-03-26 20:34:32 +0100

    67. Obsługiwanie wysyłania formularza, submit itp.

--- src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/ProjectController.java
+++ src/main/java/pl/todoapp/MarcinRogozToDoApp/controller/ProjectController.java
@@ -3,7 +3,10 @@ package pl.todoapp.MarcinRogozToDoApp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.todoapp.MarcinRogozToDoApp.model.ProjectStep;
 import pl.todoapp.MarcinRogozToDoApp.model.projection.ProjectWriteModel;
 
 @Controller
@@ -16,7 +19,17 @@ public class ProjectController {
     // To renderuje szablon z templates
     @GetMapping         // interfejs model pozwala na komunikację z kontrolerem
     String showProjects(Model model) {
+        // Ten model chcemy zachowac
         model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
+
+    // Post reaguje wtedy kiedy pojawia się parametr addStep
+    @PostMapping(params = "addStep")    // Parametr pochodzący z modelu project
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        // Pojawią się nowe kroki
+        current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
 }
