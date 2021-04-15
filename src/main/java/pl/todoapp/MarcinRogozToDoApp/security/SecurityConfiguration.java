194404c5188a51bed3854d5c69bd4f4419aba0dd
marcin1701 <marcin.rogoz1@outlook.com>
2021-04-15 17:22:17 +0200

    92. Dostarczanie autoryzacji konfiguracji  AuthenticationManagerBuilder, KeycloakAuthenticationProvider

--- src/main/java/pl/todoapp/MarcinRogozToDoApp/security/SecurityConfiguration.java
+++ src/main/java/pl/todoapp/MarcinRogozToDoApp/security/SecurityConfiguration.java
@@ -2,8 +2,12 @@ package pl.todoapp.MarcinRogozToDoApp.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -16,6 +20,23 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
     @Bean
     KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    // Nazwa roli dużymi literami
+    // Dodawanie pocesowania ról podczas pracy aplikacji
+    @Autowired
+    void configureGlobal(AuthenticationManagerBuilder auth) {
+        // Klasa służy do zmieniania ról
+        var authorityMapper = new SimpleAuthorityMapper();
+        // Role mają prefix ROLE_
+        authorityMapper.setPrefix("ROLE_");
+        // Duże litery
+        authorityMapper.setConvertToUpperCase(true);
+
+
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(authorityMapper);
+        auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 
     @Override
