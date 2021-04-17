ee568e8b04a3e17974075aee136a2d62f7465af9
marcin1701 <marcin.rogoz1@outlook.com>
2021-04-17 19:28:37 +0200

    100. Nowy wygląd strony

--- src/main/java/pl/todoapp/MarcinRogozToDoApp/MarcinRogozToDoAppApplication.java
+++ src/main/java/pl/todoapp/MarcinRogozToDoApp/MarcinRogozToDoAppApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 //import org.springframework.validation.Validator;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Validator;
 
